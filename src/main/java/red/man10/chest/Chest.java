602a12b5332239771df97bcd8be5a21edfeda057
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-22 06:03:14 +0900

    Initial commit

---
+++ src/main/java/red/man10/chest/Chest.java
@@ -0,0 +1,17 @@
+package red.man10.chest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
